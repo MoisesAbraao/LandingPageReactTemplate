bdd5c8f5061151f616f129a120071e11173c6385
moisesabraaoso <moisesdirt_bmx@hotmail.com>
2020-04-10 18:13:06 -0300

    First commit

---
+++ src/components/home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './home.css';
+
+function Home() {
+    return (
+        <div className="HomePage">
+            <header className="HomeHeader">
+                <div className="banner-text">
+                    <h1>Minha Marca Aqui</h1>
+                </div>
+            </header>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/components/portfolio/Portfolio.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './portfolio.css'
+
+function Portfolio() {
+    return(
+        <div className="PortfolioPage">
+            <header className="PortfolioHeader">
+                <div className="banner-text">
+                    <h1>Portfolio</h1>
+                </div>
+            </header>
+        </div>
+    );
+}
+
+export default Portfolio;
